aee4f7c32f3e2bbd017b34f7a360da2fb9fca7d8
Luizzz1114 <cortesialuis1114@gmail.com>
2026-04-27 14:01:40 -0400

    chore: remove timeout setting from axios instance

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,7 +3,6 @@ import router from '@/router';
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  timeout: 10000,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
